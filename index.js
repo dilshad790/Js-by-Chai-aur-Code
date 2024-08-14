95aab449aba3f1c92d87df27d08c8b81a45ee0d3
Md. Dilshad Alam <dilshadalamues786@gmail.com>
2024-08-15 02:01:08 +0530

    day2

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Hello");
+let emailId="abcd@gamail.com";
+const accountId="Andsj";
+var cityName="Delhi"; // global variable
+state="bihar"; // global variable
+let phonumber;
+console.table([emailId,accountId,cityName,state,phonumber]);
