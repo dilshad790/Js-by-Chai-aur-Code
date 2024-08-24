5f07f40e9569fd8603859c7a4e9d1e9e31043d63
Md. Dilshad Alam <dilshadalamues786@gmail.com>
2024-08-24 17:52:11 +0530

    Basic Part 1 completed

---
+++ Basic Part1/07_date.js
@@ -0,0 +1,41 @@
+// const date=new Date();
+// console.log(date);
+// console.log(typeof date);
+// // console.log(date.getDate());
+// // console.log(date.getDay());
+// console.log(date.getMonth()+1);
+
+// console.log(date.getFullYear());
+// console.log(date.getHours());
+// console.log(date.getTime());
+// console.log(date.toLocaleString());
+// console.log(date.toDateString());
+// console.log(date.toISOString());
+// console.log(date.toString());
+
+// let newDate=new Date(2024,7,23); // remb month start from 0
+// console.log(newDate.toDateString());
+// let date2=new Date(2024,7,23,5,20);
+// console.log(date2.toLocaleString());
+
+// for date type -mm dd yy
+// let date2=new Date("2024-01-23");
+// console.log(date.toLocaleString());
+// console.log(date.toDateString());
+
+
+// 0th month=jan
+// 0th day=sunday
+// Task 1
+// Create a Date object for the date: Feb 20, 2012, 3:12am. The time zone is local.
+let date=new Date(2012,1,20,3,12);
+console.log(date.toLocaleString());
+
+date=new Date(2012,0,3);
+console.log(date.getDay());
+
+
+
+
+
+
